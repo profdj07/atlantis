2c38bbb29c8fb88c728aaacd9b36ea5be71fe989
profdj07 <102644544+profdj07@users.noreply.github.com>
2023-01-06 18:31:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_ami_copy" "test" {
+resource "aws_ami_copy" "test12" {
   name              = "terraform-example"
   description       = "A copy of ami-xxxxxxxx"
   source_ami_id     = "ami-06878d265978313ca"
